922a5313a2d0ec473ac5c00b80b174fe404d3456
abhijitg2003 <112255107+abhijitg2003@users.noreply.github.com>
2023-07-05 12:05:33 +0530

    Add files via upload

---
+++ Nibble_swap.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner scan = new Scanner(System.in);
+
+
+	    
+	    // If integer is given 
+	    /*
+	    int givenNumber = scan.nextInt();
+	    String givenByte = Integer.toBinaryString(givenNumber);
+	    
+	    
+	    if(givenByte.length() < 8)
+	    {
+	        int l = 8 - givenByte.length();
+	        
+	        while(l>0)
+	        {
+	            givenByte = "0" + givenByte;
+	            l--;
+	        }
+	    }
+	    */
+
+
+
+
+	    
+	    String givenByte = scan.next();
+	    
+	    if(givenByte.length() != 8)
+	    {
+	        System.out.println("Invalid input.");
+	    }
+	    else {
+	        
+	        String s1 = givenByte.substring(0,4);
+	        String s2 = givenByte.substring(4,8);
+	        
+	        String swapped = s2 + s1;
+	        System.out.println(Integer.parseInt(swapped,2));
+	    }
+		
+
+        
+	}
+}
